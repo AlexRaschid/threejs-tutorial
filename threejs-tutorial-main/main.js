12214d37970ca7e43aba0973f3c70937de84f78b
AlexRaschid <AlexRaschid@users.noreply.github.com>
2024-02-23 11:18:27 -0500

    adjusted camera + canvas to be browser responsive
    removed white margins

--- threejs-tutorial-main/main.js
+++ threejs-tutorial-main/main.js
@@ -1,4 +1,5 @@
-import * as THREE from 'three'; //   https://threejs.org/docs/
+import * as THREE from 'three'; //Docs: https://threejs.org/docs/
+import './style.css';
 
 //Scene
 const scene = new THREE.Scene();
@@ -12,18 +13,50 @@ const material = new THREE.MeshStandardMaterial({
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
+//Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+
 //Light
-const light = new THREE.PointLight(0xffffff, 45 , 100);
-light.position.set(0, 10, 10);
+const light = new THREE.PointLight(0xffffff, 70 , 100);
+light.position.set(0, 10, 10);//x, y, z
 scene.add(light);
 
 //Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600);
-camera.position.z = 10;
+const camera = new THREE.PerspectiveCamera(
+  45, 
+  sizes.width / sizes.height, 
+  0.1, 
+  100
+);
+camera.position.z = 20;
 scene.add(camera);
 
 //Renderer
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+
+//Resize
+window.addEventListener("resize", () => {
+  //Update Sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  //Update Camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+})
+
+//Animation, rerendering the scene
+const loop = () => {
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(loop);
+};
+loop();
