b5dfacdd5ca89a5bb092be49b1d54ab40e12da24
AlexRaschid <AlexRaschid@users.noreply.github.com>
2024-02-26 05:09:57 -0500

    small visual style to sphere

--- threejs-tutorial-main/main.js
+++ threejs-tutorial-main/main.js
@@ -10,6 +10,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
+  roughness: 0.6,
 });
 
 const mesh = new THREE.Mesh(geometry, material);
