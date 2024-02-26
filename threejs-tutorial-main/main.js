9711cb7a2eda84e07b9e30739d224d89ec5e10da
AlexRaschid <AlexRaschid@users.noreply.github.com>
2024-02-26 05:07:30 -0500

    changes colors on mouseDown

--- threejs-tutorial-main/main.js
+++ threejs-tutorial-main/main.js
@@ -12,8 +12,8 @@ const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
 });
 
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 //Sizes
 const sizes = {
@@ -81,6 +81,25 @@ loop();
 //Timeline
 
 const tl = gsap.timeline({defaults: {duration: 1}});
-tl.fromTo(sphere.scale, {z:0,x:0,y:0}, {z:1,x:1,y:1},);
+tl.fromTo(mesh.scale, {z:0,x:0,y:0}, {z:1,x:1,y:1},);
 tl.fromTo("nav", {y:"-100%"}, {y:"0%"});
 tl.fromTo('.title', {opacity: 0}, {opacity: 1});
+
+
+//Mouse Animation Color
+let mouseDown = false;
+let rgb = [];
+window.addEventListener('mousedown', () => (mouseDown = true));
+window.addEventListener('mouseup', () => (mouseDown = false));
+
+window.addEventListener('mousemove', (e) => {
+  if(mouseDown){
+    const rgb = {
+      r: Math.round(e.clientX / sizes.width),
+      g: Math.round(e.clientY / sizes.height),
+      b: 0.6,
+    };
+  
+    gsap.to(mesh.material.color, rgb);
+  }
+});
